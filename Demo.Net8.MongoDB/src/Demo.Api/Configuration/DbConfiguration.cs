fd6f1a8fde7a489ee8f538494363d1c1f6c9137c
wex-maianatanael <natanael.maia@wexinc.com>
2024-07-04 16:22:40 -0300

    :memo: adds comments

--- Demo.Net8.MongoDB/src/Demo.Api/Configuration/DbConfiguration.cs
+++ Demo.Net8.MongoDB/src/Demo.Api/Configuration/DbConfiguration.cs
@@ -9,6 +9,10 @@ namespace Demo.Api.Configuration
             var connString = config.GetConnectionString("MongoDB");
 
             services.AddEntityFrameworkMongoDB();
+
+            // info: this way both database and collections (documents) weren't created if not existed.
+            // todo: must implement options to use the methods GetCollection and GetDatabase to create them if not exists.
+            // todo: we can possibly need to create some indexes too, this config method would be the right place to do it.
             services.AddMongoDB<DemoDBContext>(connString, "my_sample_db");
 
             return services;
